ab1a74bbb06423300add420543d4ee472b46f11f
suhyeonP <ahah12k@gmail.com>
2022-09-11 14:50:20 +0900

    :test_tube:

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ export default function App() {
           h1, h2, h3, h4, h5, h6 {
             font-size: 1em;
             font-weight: normal;
-            margin: 0; /* or ‘0 0 1em’ if you’re so inclined */
+            margin: 0; /* or ‘0 0 1em’ if you’re so  inclined */
           }
         `}
       />
